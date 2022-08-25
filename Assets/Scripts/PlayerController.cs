b09c8a5d89aa1ddf1e4f1b662d64ffc24d24d60d
Lucerito <51010556+Lucerito17@users.noreply.github.com>
2022-08-25 08:32:11 -0500

    Tercero

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,114 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float veloci = 6;
+    public float velocity = 10;
+    public float velocityJump = 8;
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator animator;
+    Collider2D cl;
+
+    const int ANIMATION_CORRER = 2;
+    const int ANIMATION_QUIETO = 0;
+    const int ANIMATION_CAMINAR = 1;
+    const int ANIMATION_ATACAR = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Iniciando Juego");
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        cl = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Caminar();
+        Correr();
+        Saltar();
+        Atacar();
+        GirarAnimacion();
+        CheckGround();
+    }
+
+    private void Caminar()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)){
+            rb.velocity = new Vector2(veloci, rb.velocity.y);
+            ChangeAnimation(ANIMATION_CAMINAR);
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            ChangeAnimation(ANIMATION_QUIETO);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            rb.velocity = new Vector2(-veloci, rb.velocity.y);
+            ChangeAnimation(ANIMATION_CAMINAR);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            ChangeAnimation(ANIMATION_QUIETO);
+        }
+    }
+    private void Correr()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.X))
+        {
+            rb.velocity = new Vector2(velocity, rb.velocity.y);
+            ChangeAnimation(ANIMATION_CORRER);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.X))
+        {
+            rb.velocity = new Vector2(-velocity, rb.velocity.y);
+            ChangeAnimation(ANIMATION_CORRER);
+        }
+    }
+    private void Saltar()
+    {
+        animator.SetFloat("jumpVelocity", rb.velocity.y);
+        if(!cl.IsTouchingLayers(LayerMask.GetMask("ground"))){return;}
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, velocityJump);
+        }
+    }
+    private void Atacar()
+    {
+        if (Input.GetKey(KeyCode.Z)){
+            ChangeAnimation(ANIMATION_ATACAR);
+        }
+    }
+    private void GirarAnimacion()
+    {
+        if(rb.velocity.x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if(rb.velocity.x > 0)
+        {
+            sr.flipX = false;
+        }
+    }
+    private void ChangeAnimation(int animation)
+    {
+        animator.SetInteger("Estado", animation);
+    }
+    private void CheckGround()
+    {
+        if(cl.IsTouchingLayers(LayerMask.GetMask("ground")))
+        {
+            animator.SetBool("isGround", true);
+        }
+        else
+        {
+            animator.SetBool("isGround", false);
+        }
     }
 }
